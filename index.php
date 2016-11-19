394ee99b81a9d67b54f3e8a7bcf30d195ce1dc01
kosomi <kosomi@yahoo.com>
2016-11-19 13:33:19 -0800

    first commit

--- index.php
+++ index.php
@@ -1,38 +1,125 @@
 <?php
- 
-// Connect Server
-$conn = new mysqli('localhost', 'root', 'apmsetup', 'mydb');
-
-// Create Database
-$sql = 'CREATE DATABASE mydb';
-$conn->query($sql);
-
-//CREATE TABLE
-$sql = 'CREATE TABLE board_free(  
-	b_no INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	b_title INT(30) NOT NULL,
-	b_content TEXT NOT NULL,
-	b_date DATETIME NOT NULL,
-	b_hit INT UNSIGNED NOT NULL,
-	b_id VARCHAR(30) NOT NULL,
-	b_password VARCHAR(100) NOT NULL
-)';
-$conn->query($sql); 
-
-// Select from board_free
-$sql = 'SELECT count(*) as cnt FROM board_free';
-$result = $conn->query($sql);
-$row	= $result->fetch_assoc();
-$allPost = $row['cnt'];
-
-if(empty($allPost)) {
-	$emptyData = '<tr><td class="textCenter" colspan="5"> 글이 존재하지 않습니다. </td></tr>';
-} else {
-
-};
-
- 
-
+	require_once("../dbconfig.php");
+	// Create Database
+	$sql = 'CREATE DATABASE board_free';
+	$db->query($sql);
+	// Create Table
+	$sql = 'create table board_free (
+			b_no int unsigned not null primary key auto_increment,
+			b_title varchar(100) not null,
+			b_content text not null,
+			b_date datetime not null,
+			b_hit int unsigned not null default 0,
+			b_id varchar(20) not null,
+			b_password varchar(100) not null
+			)';
+	$db->query($sql);
+	// 
+	// 
+	
+	/* 페이징 시작 */
+	//페이지 get 변수가 있다면 받아오고, 없다면 1페이지를 보여준다.
+	if(isset($_GET['page'])) {
+		$page = $_GET['page'];
+	} else {
+		$page = 1;
+	}
+	
+	/* 검색 시작 */
+	
+	if(isset($_GET['searchColumn'])) {
+		$searchColumn = $_GET['searchColumn'];
+		$subString .= '&amp;searchColumn=' . $searchColumn;
+	}
+	if(isset($_GET['searchText'])) {
+		$searchText = $_GET['searchText'];
+		$subString .= '&amp;searchText=' . $searchText;
+	}
+	
+	if(isset($searchColumn) && isset($searchText)) {
+		$searchSql = ' where ' . $searchColumn . ' like "%' . $searchText . '%"';
+	} else {
+		$searchSql = '';
+	}
+	
+	/* 검색 끝 */
+	
+	$sql = 'select count(*) as cnt from board_free' . $searchSql;
+	$result = $db->query($sql);
+	$row = $result->fetch_assoc();
+	
+	$allPost = $row['cnt']; //전체 게시글의 수
+	
+	if(empty($allPost)) {
+		$emptyData = '<tr><td class="textCenter" colspan="5">글이 존재하지 않습니다.</td></tr>';
+	} else {
+		$onePage = 15; // 한 페이지에 보여줄 게시글의 수.
+		$allPage = ceil($allPost / $onePage); //전체 페이지의 수
+		
+		if($page < 1 && $page > $allPage) {
+?>
+			<script>
+				alert("존재하지 않는 페이지입니다.");
+				history.back();
+			</script>
+<?php
+			exit;
+		}
+	
+		$oneSection = 10; //한번에 보여줄 총 페이지 개수(1 ~ 10, 11 ~ 20 ...)
+		$currentSection = ceil($page / $oneSection); //현재 섹션
+		$allSection = ceil($allPage / $oneSection); //전체 섹션의 수
+		
+		$firstPage = ($currentSection * $oneSection) - ($oneSection - 1); //현재 섹션의 처음 페이지
+		
+		if($currentSection == $allSection) {
+			$lastPage = $allPage; //현재 섹션이 마지막 섹션이라면 $allPage가 마지막 페이지가 된다.
+		} else {
+			$lastPage = $currentSection * $oneSection; //현재 섹션의 마지막 페이지
+		}
+		
+		$prevPage = (($currentSection - 1) * $oneSection); //이전 페이지, 11~20일 때 이전을 누르면 10 페이지로 이동.
+		$nextPage = (($currentSection + 1) * $oneSection) - ($oneSection - 1); //다음 페이지, 11~20일 때 다음을 누르면 21 페이지로 이동.
+		
+		$paging = '<ul>'; // 페이징을 저장할 변수
+		
+		//첫 페이지가 아니라면 처음 버튼을 생성
+		if($page != 1) { 
+			$paging .= '<li class="page page_start"><a href="./index.php?page=1' . $subString . '">처음</a></li>';
+		}
+		//첫 섹션이 아니라면 이전 버튼을 생성
+		if($currentSection != 1) { 
+			$paging .= '<li class="page page_prev"><a href="./index.php?page=' . $prevPage . $subString . '">이전</a></li>';
+		}
+		
+		for($i = $firstPage; $i <= $lastPage; $i++) {
+			if($i == $page) {
+				$paging .= '<li class="page current">' . $i . '</li>';
+			} else {
+				$paging .= '<li class="page"><a href="./index.php?page=' . $i . $subString . '">' . $i . '</a></li>';
+			}
+		}
+		
+		//마지막 섹션이 아니라면 다음 버튼을 생성
+		if($currentSection != $allSection) { 
+			$paging .= '<li class="page page_next"><a href="./index.php?page=' . $nextPage . $subString . '">다음</a></li>';
+		}
+		
+		//마지막 페이지가 아니라면 끝 버튼을 생성
+		if($page != $allPage) { 
+			$paging .= '<li class="page page_end"><a href="./index.php?page=' . $allPage . $subString . '">끝</a></li>';
+		}
+		$paging .= '</ul>';
+		
+		/* 페이징 끝 */
+		
+		
+		$currentLimit = ($onePage * $page) - $onePage; //몇 번째의 글부터 가져오는지
+		$sqlLimit = ' limit ' . $currentLimit . ', ' . $onePage; //limit sql 구문
+		
+		$sql = 'select * from board_free' . $searchSql . ' order by b_no desc' . $sqlLimit; //원하는 개수만큼 가져온다. (0번째부터 20번째까지
+		$result = $db->query($sql);
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -42,8 +129,7 @@ if(empty($allPost)) {
 	<link rel="stylesheet" href="./css/normalize.css" />
 	<link rel="stylesheet" href="./css/board.css" />
 </head>
-<body style='padding:20px'>
-	<?php echo $allPost ?> Topics
+<body>
 	<article class="boardArticle">
 		<h3>자유게시판</h3>
 		<div id="boardList">
@@ -59,19 +145,19 @@ if(empty($allPost)) {
 					</tr>
 				</thead>
 				<tbody>
-						<?php 
-							if(isset($emptyData)){
-								echo $emptyData;
-							} else { 
-								while($row = $result->fetch_assoc())
-								{
-									$datetime = explode(' ', $row['b_date']);
-									$date = $datetime[0];
-									$time = $datetime[1];
-									if($date == Date('Y-m-d'))
-										$row['b_date'] = $time;
-									else
-										$row['b_date'] = $date;
+						<?php
+						if(isset($emptyData)) {
+							echo $emptyData;
+						} else {
+							while($row = $result->fetch_assoc())
+							{
+								$datetime = explode(' ', $row['b_date']);
+								$date = $datetime[0];
+								$time = $datetime[1];
+								if($date == Date('Y-m-d'))
+									$row['b_date'] = $time;
+								else
+									$row['b_date'] = $date;
 						?>
 						<tr>
 							<td class="no"><?php echo $row['b_no']?></td>
@@ -82,7 +168,7 @@ if(empty($allPost)) {
 							<td class="date"><?php echo $row['b_date']?></td>
 							<td class="hit"><?php echo $row['b_hit']?></td>
 						</tr>
-						<?php 
+						<?php
 							}
 						}
 						?>
