45f23ba8227e2b4892f4a96dac1cc05e51372187
kosomi <kosomi@yahoo.com>
2016-11-20 12:24:29 -0800

    Strong Basic

---
+++ index_basic.php
@@ -0,0 +1,117 @@
+
+<?php
+	require_once("../dbconfig.php");
+	// Create Database
+	$sql = 'CREATE DATABASE board_free';
+	$db->query($sql);
+	// Create Table
+	$sql = 'create table board_free (
+			b_no int unsigned not null primary key auto_increment,
+			b_title varchar(100) not null,
+			b_content text not null,
+			b_date datetime not null,
+			b_hit int unsigned not null default 0,
+			b_id varchar(20) not null,
+			b_password varchar(100) not null
+			)';
+	$db->query($sql);
+	// 
+	// 
+	
+	/* 페이징 시작 */
+	//페이지 get 변수가 있다면 받아오고, 없다면 1페이지를 보여준다.
+	if(isset($_GET['page'])) {
+		$page = $_GET['page'];
+	} else {
+		$page = 1;
+	} 
+	
+	$sql = 'select count(*) as cnt from board_free' . $searchSql;
+	$result = $db->query($sql);
+	$row = $result->fetch_assoc();
+	
+	$allPost = $row['cnt']; //전체 게시글의 수
+	
+	if(empty($allPost)) {
+		$emptyData = '<tr><td class="textCenter" colspan="5">글이 존재하지 않습니다.</td></tr>';
+	} else { 
+		 
+		$sql = 'select * from board_free'; //원하는 개수만큼 가져온다. (0번째부터 20번째까지
+		$result = $db->query($sql);
+	}
+?> 
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>자유게시판 | Kurien's Library</title>
+	<link rel="stylesheet" href="./css/normalize.css" />
+	<link rel="stylesheet" href="./css/board.css" />
+</head>
+<body>
+	<article class="boardArticle">
+		<h3>자유게시판</h3>
+		<div id="boardList">
+			<table>
+				<caption class="readHide">자유게시판</caption>
+				<thead>
+					<tr>
+						<th scope="col" class="no">번호</th>
+						<th scope="col" class="title">제목</th>
+						<th scope="col" class="author">작성자</th>
+						<th scope="col" class="date">작성일</th>
+						<th scope="col" class="hit">조회</th>
+					</tr>
+				</thead>
+				<tbody>
+						<?php
+						if(isset($emptyData)) {
+							echo $emptyData;
+						} else {
+							while($row = $result->fetch_assoc())
+							{
+								$datetime = explode(' ', $row['b_date']);
+								$date = $datetime[0];
+								$time = $datetime[1];
+								if($date == Date('Y-m-d'))
+									$row['b_date'] = $time;
+								else
+									$row['b_date'] = $date;
+						?>
+						<tr>
+							<td class="no"><?php echo $row['b_no']?></td>
+							<td class="title">
+								<a href="./view.php?bno=<?php echo $row['b_no']?>"><?php echo $row['b_title']?></a>
+							</td>
+							<td class="author"><?php echo $row['b_id']?></td>
+							<td class="date"><?php echo $row['b_date']?></td>
+							<td class="hit"><?php echo $row['b_hit']?></td>
+						</tr>
+						<?php
+							}
+						}
+						?>
+				</tbody>
+			</table>
+			<div class="btnSet">
+				<a href="./write.php" class="btnWrite btn">글쓰기</a>
+			</div>
+			<div class="paging">
+				<?php echo $paging ?>
+			</div>
+			<div class="searchBox">
+				<form action="./index.php" method="get">
+					<select name="searchColumn">
+						<option <?php echo $searchColumn=='b_title'?'selected="selected"':null?> value="b_title">제목</option>
+						<option <?php echo $searchColumn=='b_content'?'selected="selected"':null?> value="b_content">내용</option>
+						<option <?php echo $searchColumn=='b_id'?'selected="selected"':null?> value="b_id">작성자</option>
+					</select>
+					<input type="text" name="searchText" value="<?php echo isset($searchText)?$searchText:null?>">
+					<button type="submit">검색</button>
+				</form>
+			</div>
+		</div>
+	</article>
+</body>
+</html>
